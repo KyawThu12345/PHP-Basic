6a3f893981275f984e9dbfebe4e9bc9514e3f86d
KyawThu12345 <108884741+KyawThu12345@users.noreply.github.com>
2023-05-10 17:24:46 +0630

    Add files via upload

---
+++ switch_Statement.php
@@ -0,0 +1,9 @@
+<?php 
+$num = -1;
+switch($num){
+     case 2: echo "Number = 2";break;
+     case 1: echo "Number = 1";break;
+     case 0: echo "Number = 0";break;
+     default :echo "Number is Invalid.";
+}
+?>
