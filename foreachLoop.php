10d6eecf8462eab85e5b289b9ff18cba5978d60b
KyawThu12345 <108884741+KyawThu12345@users.noreply.github.com>
2023-05-11 17:15:00 +0630

    Add files via upload

---
+++ foreachLoop.php
@@ -0,0 +1,13 @@
+<?php 
+$num = array(1,2,3,4,5);
+foreach($num as $number){
+    echo $number."<br>";
+}
+echo "<br>";
+$num = array("One"=>1,"Second"=>2,"Third"=>3,"Fourth"=>4,"Five"=>5);
+foreach($num as $key=>$val){
+    echo $val."<br>";
+}
+
+
+?>
